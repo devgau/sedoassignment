afc0a0adf16ef8e622c9b0ee5fef1ffdcb29dc0a
Devansh Gauri <degauri@cisco.com>
2024-12-11 22:45:57 +0000

    added logs for main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from flask import Flask, render_template,session, request, redirect, url_for, fl
 from werkzeug.security import generate_password_hash, check_password_hash
 from home import home_blueprint, room_gallery, get_db
 from admin import admin_blueprint,admin_home
-# from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-
+import logging
 import sqlite3
 
 # Create a Flask application instance
@@ -13,6 +12,14 @@ app = Flask(__name__, static_folder='static\\')
 app.secret_key = 'strong_key'
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint)
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s',
+                    filename='auditlog.log',
+                    filemode='w')
+
+logger = logging.getLogger()
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 # List of security questions for user registration and recovery
 security_questions = [
@@ -56,15 +63,19 @@ def login():
                         session['name'] = result[2]
                         session['user_type'] = 'regular'
                         session['logged_in'] = True
+                        logger.info(f'User login successful: {emailaddress} (User ID: {result[0]})')
                         return redirect(url_for('home.room_gallery'))
                     else:
                         flash('Invalid email or password', 'error')
+                        logger.info(f'Failed login attempt for user: {emailaddress}')
                         return redirect(url_for('login'))
                 else:
                     flash('Invalid email or password', 'error')
+                    logger.warning(f'Failed login attempt for user: {emailaddress}')
                     return redirect(url_for('login'))
                 
-            except:
+            except Exception as e:
+                logger.error(f'Error during login attempt for user: {emailaddress}. Error: {str(e)}')
                 return redirect(url_for('login'))
 
             
@@ -80,15 +91,19 @@ def login():
                         session['user_type'] = 'admin'
                         session['username'] = result[0]
                         session['logged_in'] = True
+                        logger.info(f'Admin login successful: {emailaddress} (Admin ID: {result[0]})')
                         return redirect(url_for('admin.admin_home'))
                     else:
                         flash('Invalid username or password', 'error')  
+                        logger.warning(f'Failed admin login attempt for user: {emailaddress}')
                         return redirect(url_for('login'))
                 else:
                     flash('Invalid email or password', 'error') 
+                    logger.warning(f'Failed admin login attempt for user: {emailaddress}')
                     return redirect(url_for('login'))
                 
-            except:
+            except Exception as e:
+                logger.error(f'Error during admin login attempt for user: {emailaddress}. Error: {str(e)}')
                 return redirect(url_for('login'))
 
     return render_template('login.html', security_questions=security_questions)
@@ -108,14 +123,22 @@ def signup():
         recovery_answer = request.form['security_answer']
         status = 'Active'
 
-    cursor.execute("""
-            INSERT INTO user (password, recoveryQuestion,recoveryAnswer, name,email, status )
-            VALUES (?,?, ?,?,?,?)
-        """.format(), (generate_password_hash(password), recovery_question, recovery_answer,name, email, status))
-    
-    db.commit()
+        cursor.execute('SELECT email FROM user WHERE email = ?', (email,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            logger.warning(f"Attempt to register with existing email: {email}")
+            flash('Attempt to register with existing email', 'error')
+            return render_template('login.html', security_questions=security_questions)
+        else:
+            cursor.execute("""
+                    INSERT INTO user (password, recoveryQuestion, recoveryAnswer, name, email, status)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                """, (generate_password_hash(password), recovery_question, recovery_answer, name, email, status))
 
-    return render_template('login.html', security_questions=security_questions)
+            db.commit()
+            logger.info(f'New user signed up: {email} (Name: {name})')
+
+            return render_template('login.html', security_questions=security_questions)
 
 # Route for handling forgot password functionality
 @app.route('/forgot_password', methods=['GET', 'POST'])
@@ -137,16 +160,17 @@ def forgot_password():
 
             if recovery_question == stored_recovery_question and recovery_answer == stored_recovery_answer:
                 user_authentication = 'true'
-            
+                logger.info(f'Password recovery initiated for user: {email}')
                 return render_template('login.html', user_authentication = user_authentication, reset_email = email)
 
             else:
                 user_authentication = 'false'
-                
+                logger.warning(f'Failed password recovery attempt for user: {email}')
                 return render_template('login.html', user_authentication = user_authentication, security_questions=security_questions)
             
         else: 
             user_authentication = 'false'
+            logger.warning(f'Password recovery attempted for non-existing user: {email}')
             return render_template('login.html', user_authentication = user_authentication, security_questions=security_questions)
 
 
@@ -163,6 +187,7 @@ def reset_password():
         db.commit()
 
         reset_done ='true'
+        
         return render_template('login.html', security_questions=security_questions, reset_done = reset_done)
 
 
